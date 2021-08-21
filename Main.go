217d672d3ba5e8cad33f3c052a9b9bba5fd3a8eb
Keval <keval@acquire.io>
2021-08-21 12:39:13 +0530

    first commit

---
+++ Main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func main() {
+	clientOptions := options.Client().
+		ApplyURI("mongodb+srv://kevalgo:kevalgo@cluster0.wvcmr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	databases, err := client.ListDatabaseNames(ctx, bson.M{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(databases)
+}
